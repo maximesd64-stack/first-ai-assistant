8872c912a287a92ea9824131ce7af19a6604bec7
maximesmitchduny-cyber <maximesmitchduny@gmail.com>
2026-09-24 10:27:59 +0200

    Update assistant

--- Challenge.py
+++ Challenge.py
@@ -2,28 +2,28 @@ name = input("What is your name? ")
 degree = input("What is your degree? ")
 print("Welcome", name, "! You are studying", degree)
 
-#  grades = float(input("what is ur grades?"))
-#      if grades >= 15:
-#          print("Perfect")
-#      elif grades >=10:
-#          print("u pass good job")
-#      else:
-#          print("practice more")
-#  grade1 = float(input("what is ur first grade ?"))
-#  grade2 = float(input("what is ur first grade ?"))
-#  moy_grade = (grade1 + grade2) / 2
-#  print(moy_grade)
+  grades = float(input("what is ur grades?"))
+      if grades >= 15:
+          print("Perfect")
+      elif grades >=10:
+          print("u pass good job")
+      else:
+          print("practice more")
+  grade1 = float(input("what is ur first grade ?"))
+  grade2 = float(input("what is ur first grade ?"))
+  moy_grade = (grade1 + grade2) / 2
+  print(moy_grade)
 
-# stuying_time = int(input("How many hours do you work at home? "))
+ stuying_time = int(input("How many hours do you work at home? "))
 
-# if stuying_time == 0:
-#     print("You need to work at home!!!")
+ if stuying_time == 0:
+     print("You need to work at home!!!")
 
-# elif stuying_time <= 2:
-#     print("Continue like that")
+ elif stuying_time <= 2:
+     print("Continue like that")
 
-# else:
-#     print("Take some rest....")
+ else:
+     print("Take some rest....")
 
 choise = input(
     "What u want to do ?\n"
